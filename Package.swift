3649131faa05f0b095994899347552ae725dff6d
vberegovoy <valik.beregovoy@gmail.com>
2020-08-25 20:38:59 +0300

    Added swift package

---
+++ Package.swift
@@ -0,0 +1,12 @@
+// swift-tools-version:5.0
+
+import PackageDescription
+
+
+
+let package = Package(
+  name: "Cloudipsp",
+  platforms: [.iOS(.v10)],
+  products: [.library(name: "Cloudipsp", targets: ["Cloudipsp"])],
+  targets: [.target(name: "Cloudipsp", path: "Pod/Classes", publicHeadersPath: "")]
+)
